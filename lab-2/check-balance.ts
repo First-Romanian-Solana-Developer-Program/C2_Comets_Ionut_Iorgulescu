4b4b813c8e1a3c422f7c4ea8bf0d9cd6fa766382
Ionut Iorgulescu <ionut.iorgulescu87@gmail.com>
2024-10-17 17:14:04 +0300

    feat: add lab-2

---
+++ lab-2/check-balance.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { airdropIfRequired } from "@solana-developers/helpers";
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+console.log("Connected to devnet!", connection.rpcEndpoint);
+
+const publicKey = new PublicKey("65iHwGLu5SJjGfKRACwn28f4PJ15uWureEaFTUPKiBQ2");
+
+const balanceInLamports = await connection.getBalance(publicKey);
+
+console.log("Accounts's balance in lamports:", balanceInLamports);
+
+console.log("Airdropping 1 SOL to Account...");
+
+await airdropIfRequired(
+  connection,
+  publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.5 * LAMPORTS_PER_SOL
+);
+
+console.log("Done airdropping!");
+
+const newBalanceInLamports = await connection.getBalance(publicKey);
+
+console.log("Accounts's new balance in lamports:", newBalanceInLamports);
